10cd7ab6b46053b0f1a24477048494e66dde924c
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-20 22:51:06 +0330

    fix: make cinematic controls compatible with RN types

--- src/app/content/[slug].tsx
+++ src/app/content/[slug].tsx
@@ -1087,7 +1087,11 @@ const styles = StyleSheet.create({
     backgroundColor: palette.black,
   },
   cinemaControls: {
-    ...StyleSheet.absoluteFillObject,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
     zIndex: 4,
     justifyContent: 'space-between',
     paddingHorizontal: 11,
