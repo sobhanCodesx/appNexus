abf3da3a29510588353ec5965108426345b6e08d
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-20 11:38:55 +0330

    perf(native): show global offline state

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,6 +6,7 @@ import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import { ConnectivityBanner } from '@/components/system/connectivity-banner';
 import { palette } from '@/design';
 import { useNotificationNavigation } from '@/hooks/use-notification-navigation';
 
@@ -33,6 +34,7 @@ export default function RootLayout() {
           <Stack.Screen name="search" />
           <Stack.Screen name="content/[slug]" />
         </Stack>
+        <ConnectivityBanner />
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
